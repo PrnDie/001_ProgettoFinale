45307555398103a42844ad493a97fcbafaf98a2d
Diego <Diego@172.23.163.74>
2019-06-20 17:36:01 +0200

    Agg

--- 001_PrgSpring/src/main/java/com/example/demo/Control.java
+++ 001_PrgSpring/src/main/java/com/example/demo/Control.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,32 +17,35 @@ public class Control
 	@Autowired
 	private Serv serv;
 
+	@RequestMapping("/")
+	public String Start()
+	{
+		return "Benvenuto, ";
+	}
+	
 	@GetMapping("/media")
 	public String Prova() throws Exception
 	{
 		return serv.media();
 	}
 	
-	@GetMapping("/dati")
-	public String Dati() throws Exception
+	@GetMapping("/dati")  
+    public ArrayList<Ripetitore> Dati(@RequestParam(value="filter", defaultValue="Std") String filter)
 	{
-		ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-		mapper.toString();
-		String json = mapper.writeValueAsString(serv.dati());
-		return json;
-	}
+        try {
+			return serv.Dati(filter);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return null;
+    }
 	
 	@GetMapping("/metadati")
 	public String MetaDati() throws Exception
-	{
+	{		
 		ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 		mapper.toString();
 		String json = mapper.writeValueAsString(serv.MetaDati());
-		return json;    
+		return json;   
 	}
-	
-	@RequestMapping("/")
-	public String invoke() {
-		return "Welcome ";
-	}	
 }
